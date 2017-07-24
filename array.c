a7372d7475f8ed1e37c494aa9f80a4da6d38424f
Tyler Holloway <htoyll@fb.com>
2017-07-24 01:30:21 -0700

    print out each element in an array

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  int MyNumbersArray[3] = {1, 2, 3};
+
+  // Print each element in the array
+  for(int i = 0; i < 3; i++) {
+    printf("Element[%d] = %d\n", i, MyNumbersArray[i]);
+  }
+
+}
