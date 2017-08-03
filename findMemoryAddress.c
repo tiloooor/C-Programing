18905535ffa7e72fea98f3fb1e993d6c56b38bee
Tyler Holloway <htoyll@fb.com>
2017-08-02 23:58:24 -0700

    Created a pointer

--- findMemoryAddress.c
+++ findMemoryAddress.c
@@ -4,7 +4,10 @@ int main() {
 
   int val = 30;
 
-  // Must use a hexadecimal number to display the address
-  printf("The address of val is: %x", &val);
+  int *pointer_p;
+  pointer_p = &val;
 
+  // Must use a hexadecimal number to display the address
+  printf("The address of val is: %x \n", &val);
+  printf("The address of val is %x \n", &val);
 }
